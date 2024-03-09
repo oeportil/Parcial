ee67e12233be0bcdbf4dab70bf752f0236b60a0b
JonathanAva <hpalvarez10@gmail.com>
2024-03-09 12:35:31 -0600

    a

---
+++ Parcial/Controllers/AutoresController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PARCIAL_1A.Models;
+
+namespace PARCIAL_1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly Context _contex;
+        public AutoresController(Context context)
+        {
+            _contex = context;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<Autores> listadoautores = (from l in _contex.Autores
+                                          select l).ToList();
+
+            if (listadoautores.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoautores);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult guardarAutor([FromBody] Autores autor)
+        {
+            try
+            {
+                _contex.Autores.Add(autor);
+                _contex.SaveChanges();
+                return Ok(autor);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
