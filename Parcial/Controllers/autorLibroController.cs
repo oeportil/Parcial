62605a7f0a47b68af1b99bd978b31db19f3fc194
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-03-09 12:35:02 -0600

    libro controller

---
+++ Parcial/Controllers/autorLibroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PARCIAL_1A.Models;
+
+namespace PARCIAL_1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class autorLibroController : ControllerBase
+    {
+
+        private readonly Context _contex;
+
+        public autorLibroController(Context context)
+        {
+            _contex = context;
+        }
+
+
+    }
+}
