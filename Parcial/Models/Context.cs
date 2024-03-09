3e36ec13ee65efb46976ad941ddb29c966a7303b
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-03-09 11:40:00 -0600

    Agregar archivos de proyecto.

---
+++ Parcial/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Parcial.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { 
+
+        }
+    }
+}
